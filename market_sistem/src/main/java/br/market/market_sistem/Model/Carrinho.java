9b317d92fa68b7a0f20a7003c31c8aa8e630d7a0
Daniele Querino <daniele.querino.704@ufrn.edu.br>
2024-04-14 22:48:42 -0300

    Recebendo atualizações

--- market_sistem/src/main/java/br/market/market_sistem/Model/Carrinho.java
+++ market_sistem/src/main/java/br/market/market_sistem/Model/Carrinho.java
@@ -1,6 +1,6 @@
 package br.market.market_sistem.Model;
 
-import org.springframework.context.annotation.Scope;
+
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
