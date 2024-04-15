3f7ef191184a8df87e399b7b9dbcaf6b27ba5f18
Daniele Querino <128300815+danielequerino7@users.noreply.github.com>
2024-04-14 23:08:50 -0300

    Revert "Recebendo atualizações"

--- market_sistem/src/main/java/br/market/market_sistem/Model/ProdutoDAO.java
+++ market_sistem/src/main/java/br/market/market_sistem/Model/ProdutoDAO.java
@@ -47,8 +47,8 @@ public class ProdutoDAO {
             r = p.executeQuery();
 
             while(r.next()){
-                Produto produto = new Produto(r.getInt("id_produto"), r.getFloat("preco"), r.getString("nome"), r.getString("descricao"), r.getInt("estoque"));
-                lista.add(produto);
+                Produto pr = new Produto(r.getInt("id_produto"), r.getFloat("preco"), r.getString("nome"), r.getString("descricao"), r.getInt("estoque"));
+                lista.add(pr);
             }
 
         }catch(SQLException | URISyntaxException exception){
@@ -77,8 +77,5 @@ public class ProdutoDAO {
         }
         return produto;
     }
-<<<<<<< HEAD:market_sistem/src/main/java/br/market/market_sistem/Model/ProdutoDAO.java
 
-=======
->>>>>>> 369dcf1bc4a460483bbd6f96830825a475d53d07:market_sistem/src/main/java/br/market/market_sistem/ProdutoDAO.java
 }
